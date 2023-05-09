b07d9aed5d6b5bbaa310f1c386ec8f0c27577857
5x <longhorn482653@gmail.com>
2023-05-09 17:19:36 +0800

    refactor: use langchain get_elements

--- news_breaker/article_summarizer_v2.py
+++ news_breaker/article_summarizer_v2.py
@@ -3,10 +3,9 @@ from news_breaker.browser import Browser
 from langchain.agents import initialize_agent, AgentType
 from langchain.chat_models.openai import ChatOpenAI
 from langchain import PromptTemplate
-from news_breaker.get_elements import GetElementsToolUtf8
 
 TEMPLATE = """
-1. 根據連結使用 selector: article || .fncnews-content, attributes: [innerText] 取得 innerText
+1. 根據連結使用 selector: article || .fncnews-content || .story, attributes: [innerText] 取得 innerText
 2. 以台灣慣用詞彙產生100字以內的繁體中文總結。
 
 連結：{url}
@@ -35,11 +34,7 @@ class ArticleSummarizer:
             tools = toolkit.get_tools()
             tools_by_name = {tool.name: tool for tool in tools}
             navigate_tool = tools_by_name["navigate_browser"]
-            # we use GetElementsToolUtf8 instead of GetElementsTool
-            # because we want to get utf8 text instead of ascii text
-            get_elements_tool = GetElementsToolUtf8.from_browser(
-                async_browser=browser.get_browser()
-            )
+            get_elements_tool = tools_by_name["get_elements"]
             # Initialize agent and model
             chat = ChatOpenAI(temperature=0)
             agent_chain = initialize_agent(
